741449a6d5e94c1135929f50d62f511212f3bc3d
byteyang <byteyang@tencent.com>
2023-03-01 14:32:03 +0800

    添加ByUMG插件

---
+++ Plugins/ByUMG/Source/ByUMGEditor/ByUMGEditor.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class ByUMGEditor : ModuleRules
+{
+    public ByUMGEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Plugins/ByUMG/Source/ByUMGEditor/Private/ByUMGEditor.cpp
@@ -0,0 +1,16 @@
+﻿#include "ByUMGEditor.h"
+
+#define LOCTEXT_NAMESPACE "FByUMGEditorModule"
+
+void FByUMGEditorModule::StartupModule()
+{
+}
+
+void FByUMGEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FByUMGEditorModule, ByUMGEditor)
