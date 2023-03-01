38acb684dbf19d91ddbf7e395aad07519be68ecb
byteyang <byteyang@tencent.com>
2023-03-01 11:28:06 +0800

    创建项目

---
+++ Source/ByWidget/ByWidgetGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ByWidgetGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BYWIDGET_API AByWidgetGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
